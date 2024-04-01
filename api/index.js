4394759960e94ca30ad34c53d8f40a60bbcfe327
Suhail Rezaie <suhail.rezaie1378@gmail.com>
2024-04-01 17:03:47 +0500

    cors issue

--- api/index.js
+++ api/index.js
@@ -1,3 +1,65 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const authRoute = require("./routes/auth");
+// const userRoute = require("./routes/users");
+// const postsRoute = require("./routes/posts");
+// const categoryRoute = require("./routes/categories");
+// const app = express();
+// const dotenv = require("dotenv");
+// const multer = require("multer");
+// const path = require("path");
+// var cors = require('cors')
+
+
+
+// // File Uploading using multer
+// app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "images");
+//   },
+//   filename: (req, file, cb) => {
+//     //there must be file name
+//     // cb(null, "test.jpg");
+//     cb(null, req.body.name);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("flie uploaded successfully");
+// });
+
+// //DB Connection
+// app.use(express.json());
+// dotenv.config();
+// mongoose
+//   .connect(process.env.URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
+
+// // Routes
+
+// app.use("/test",(req,res)=>{
+//   res.send("it works");
+// });
+// app.use(cors())
+// app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postsRoute);
+// app.use("/api/categories", categoryRoute);
+
+// // Server Port
+// app.listen("5600", () => {
+//   console.log("backend is running");
+// });
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
@@ -8,8 +70,7 @@ const app = express();
 const dotenv = require("dotenv");
 const multer = require("multer");
 const path = require("path");
-var cors = require('cors')
-
+const cors = require('cors'); // Import cors module correctly
 
 
 // File Uploading using multer
@@ -29,7 +90,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("flie uploaded successfully");
+  res.status(200).json("file uploaded successfully");
 });
 
 //DB Connection
@@ -44,11 +105,10 @@ mongoose
   .catch((err) => console.log(err));
 
 // Routes
-
+app.use(cors()); // Apply cors middleware here
 app.use("/test",(req,res)=>{
   res.send("it works");
 });
-app.use(cors())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postsRoute);
