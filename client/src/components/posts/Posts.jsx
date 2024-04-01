165b7285e67d3818fb775bef11e0192101d4a25b
Suhail Rezaie <suhail.rezaie1378@gmail.com>
2024-04-01 15:59:12 +0500

    posts up

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,7 +1,27 @@
+// import Post from "../post/Post";
+// import "./posts.css";
+
+// export default function Posts({ posts }) {
+//   return (
+//     <div className="posts">
+//       {posts.map((p) => (
+//         <Post key={p._id} post={p}  />
+//       ))}
+//     </div>
+//   );
+// }
+
+
 import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts({ posts }) {
+  if (!Array.isArray(posts)) {
+    // If posts is not an array, you can handle this situation accordingly.
+    // For example, you can return a message indicating that there are no posts.
+    return <div>No posts available</div>;
+  }
+
   return (
     <div className="posts">
       {posts.map((p) => (
